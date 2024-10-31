74597d12ea93ae4e2947d325abbac16d4c3e7aac
bin <bin.pan@daocloud.io>
2024-10-31 18:11:19 +0800

    bump version to v0.7.7

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -5,7 +5,7 @@ const (
 	DefaultIngressName        = "cloudshell-ingress"
 	DefaultVirtualServiceName = "cloudshell-virtualService"
 	DefaultServicePort        = 7681
-	DefaultTtydImage          = "ghcr.io/cloudtty/cloudshell:v0.7.6"
+	DefaultTtydImage          = "ghcr.io/cloudtty/cloudshell:v0.7.7"
 
 	CloudshellPodLabelKey = "cloudshell.cloudtty.io/pod-name"
 
